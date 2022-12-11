6c5d065873d10d0dfea774535fcca9995e8f7fee
Cody Le <49752325+Cody-Le@users.noreply.github.com>
2022-12-10 21:40:04 -0500

    Add files via upload

--- Neuronetwork.py
+++ Neuronetwork.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import math
 
 
 
@@ -30,7 +31,7 @@ class neuronet():
         XatStep = []
         XatStep.append(X)
         for i in range(self.n):
-            print(self.weights[i].shape)
+
             X = self.activation(np.dot(X, self.weights[i]))
             XatStep.append(X)
         #print(X)
@@ -45,15 +46,66 @@ class neuronet():
 
 
     def predict(self, x):
-        result = np.ones(len(x))
+        result = np.ones(len(x)) * 0.5
+
         for i,X in enumerate(x):
-            result[i] *= self.feed_forward((X))
-
-        return self.sigmoidLayer(result)
 
 
-    def training(self, x):
-        pass
+            result[i] *= self.feed_forward(X)[0]
+
+        return result
+
+
+    def combineTheta(self, theta1, theta2, substract = False):
+        if (substract):
+            return [theta1[0] + theta2[0], theta1[1] + theta2[1], theta1[2] + theta2[2]]
+        else:
+            return [theta1[0] - theta2[0], theta1[1] - theta2[1], theta1[2] - theta2[2]]
+
+    def thetaShape(self, theta):
+
+        return [theta[0].shape,theta[1].shape,theta[2].shape]
+
+
+    def multiplyTheta(self, theta, multiplier):
+        return [multiplier* theta[0], multiplier* theta[1],multiplier* theta[2]]
+
+
+
+    def training(self, x, y, episelon,batch, epochs):
+        costOverIteration = np.zeros(epochs)
+        for epoch in range(epochs):
+            print(epoch)
+            m = len(x)
+            n = int(math.floor(m / batch))
+            lastN = m - batch * n
+            for o in range(1, n):  # Dividing everything into epoch of batches
+
+
+
+                totalDeltaC = None
+                for i, X in enumerate(x[batch * (o - 1): batch * o]):
+
+                    y_pred, xatstep = self.feed_forward((X))
+                    if totalDeltaC != None:
+                        totalDeltaC = self.combineTheta(self.backProp(xatstep, y_pred, y[i]), totalDeltaC)
+
+                    else:
+                        totalDeltaC = self.backProp(xatstep, y_pred, y[i])
+
+                learningFactor = self.multiplyTheta(totalDeltaC,
+                                                    episelon)  # Applying in episelon into the the derivative of theta]
+
+                self.weights = self.combineTheta(self.weights, learningFactor, substract=True)
+
+
+            new_pred = self.predict(x)
+            print(self.cost_function(new_pred, y))
+
+
+
+
+    pass
 
 
 
@@ -87,8 +139,12 @@ class neuronet():
 
 
     def backProp(self,x ,y_pred ,y):
+
         dCda = self.devCost(y_pred, y) * self.devSigmoid(self.activation(x[-1])) * self.devRelu(x[-1])
-        dC1 =  dCda * self.devZ(x[-2])
+        print(dCda)
+        dC1 =  dCda * self.devZ(x[-2][:,np.newaxis])
+
+
 
         dCd2a = dCda * self.devTheta(self.weights[-1]) * self.devRelu(x[-2])
         dC2 = np.dot(x[-3][:,np.newaxis], dCd2a[np.newaxis,:])
@@ -121,7 +177,8 @@ network = neuronet(8, [4,3,1])
 
 X_scaled_train = featureScale(X_train)
 y_pred, XatTimeStep = network.feed_forward(X_scaled_train[5])
+
 #print(network.cost_function(y_pred, y_train[0]))
-#print(XatTimeStep)
+print(X_scaled_train[5], y_pred)
 costDev = network.backProp(XatTimeStep, y_pred, y_train[0])
-print(costDev)
+network.training(X_scaled_train[0:10], y_train[0:10], 0.4,3, 10)
